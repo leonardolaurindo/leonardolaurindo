f345af9501c014467de935f9528ae2d7885ec2b5
Leonardo Laurindo <leonardolaurindodev@outlook.com>
2024-08-05 13:58:10 -0300

    change route

--- src/app/api/getChatScript/route.js
+++ src/app/api/getChatScript/route.js
@@ -4,18 +4,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
     const script = `
-    (function(i,s,g,r,j,y,b,p,t,z,a){
-      a=s.createElement(r),
-      a.async=1,
-      a.src=g.concat(b,j,b,y,p,j),
-      s.head.appendChild(a),
-      a.onload = function(){opa.init(g,t,z)}
-    })(window,document,'https://atendimento.intercert.com.br','script','js','opa','/','.','6617e0937755ae29fd0608b2',\`{
-      "google_credential":"AIzaSyAhddHt7QCcfgMnSj1miK1HNq92hzHHtys",
-      "google_oauth":"181305973380-9jbl84uic2oi5nqq406gkt9jmkvd9f8u.apps.googleusercontent.com",
-      "facebook_appid":"",
-      "permitir_login_anonimo":"on"
-    }\`);
+    (function(i,s,g,r,j,y,b,p,t,z,a){a=s.createElement(r),a.async=1,a.src=g.concat(b,j,b,y,p,j),s.head.appendChild(a),a.onload = function(){opa.init(g,t,z)}})(window,document,'https://atendimento.intercert.com.br','script','js','opa','/','.','658d7cd38059d3c55382b28b','{"google_credential":"AIzaSyDde1OlXC3SRVAdhUxnINDAB2ib5smwhpU","google_oauth":"676602925127-9uank6gphvvoqq0rqnsuns1t61mopaql.apps.googleusercontent.com","facebook_appid":"","permitir_login_anonimo":""}');
   `;
 
     return new NextResponse(script, {
