e2a732cb249f73eaf4077f1f3c734c82d61f57bb
Leonardo Laurindo <leonardolaurindodev@outlook.com>
2025-09-23 10:47:38 -0300

    chore: update dependencies and refactor theme provider component
    - Updated @next/third-parties to version 15.0.3
    - Updated framer-motion to version 11.11.17
    - Updated lucide-react to version 0.468.0
    - Updated next to version 15.0.3
    - Updated next-themes to version 0.4.3
    - Updated react and react-dom to version 19
    - Updated @types/react and @types/react-dom to version 19
    - Updated eslint-config-next to version 15.0.3
    - Refactored theme provider component to use NextThemeProvider and updated import statements

--- src/app/_components/theme-provider.tsx
+++ src/app/_components/theme-provider.tsx
@@ -1,10 +1,8 @@
-'use client';
+'use client'
 
-import { ThemeProvider as NexThemeProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
+import { ThemeProvider as NextThemeProvider } from 'next-themes'
+import { type ThemeProviderProps } from 'next-themes'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-    return (
-        <NexThemeProvider {...props}>{children}</NexThemeProvider>
-    )
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>
 }
